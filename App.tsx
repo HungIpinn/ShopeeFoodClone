a23986db6ba5c2c819d0a2522fb34197cb57c156
HungIpin <hungtranngoc.work@gmail.com>
2025-09-03 08:09:28 +0700

    update todo

--- App.tsx
+++ App.tsx
@@ -5,33 +5,23 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { useState } from 'react';
+import { StatusBar, StyleSheet, Text, TextInput, useColorScheme, View } from 'react-native';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+
+  const [name, setName] = useState<string>('');
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+    <View style = {styles.container}>
+      <Text>{name}</Text>
+      <TextInput 
+      value = {name}
+      onChangeText={e => {setName(e); console.log(e);}}
+      autoCapitalize = "none"
+      maxLength={12}
+      autoCorrect = {false}
+      style = {styles.txtInput}/>
     </View>
   );
 }
@@ -39,6 +29,12 @@ function AppContent() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems:'center',
+    justifyContent :"center"
+  },
+  txtInput:{
+    borderColor: 'violet',
+    borderWidth: 1
   },
 });
 
